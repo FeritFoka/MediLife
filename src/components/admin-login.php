68971d9747bf11776bcf1da377ef64aa8804d5b1
Petrara <31998191+Petrara@users.noreply.github.com>
2021-07-03 13:00:57 +0200

    Created admin login page (#6)

---
+++ src/components/admin-login.php
@@ -0,0 +1,25 @@
+<section class="section-admin-login">
+
+    <form class="login-container admin-form" action="#">
+        <h1 aria-readonly="true">Sign in</h1>
+        <div class="admin-icon">
+            <a role="link" href="#"><i class="fa fa-key" aria-hidden="true"></i></a>
+        </div>
+        <span class="admin-panel">Admin panel</span>
+        <input role="input" class="admin-input" type="email" placeholder="Email" />
+        <input role="input" class="admin-input" type="password" placeholder="Password" />
+        <a role="link" href="#">Forgot your password?</a>
+        <button type="submit" role="button" aria-pressed="false" class="admin-button">Sign In</button>
+    </form>
+
+    <div class="admin-home-container">
+        <div class="admin-home">
+            <div class="admin-home-panel">
+                <h1 aria-readonly="true">Hello, Admin!</h1>
+                <p aria-readonly="true">Enter your login data and start journey with us or just back home</p>
+                <button role="button" aria-pressed="false" class="admin-button admin-home-button">Home</button>
+            </div>
+        </div>
+    </div>
+
+</section>
