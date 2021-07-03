9491688ecf35a66b23a9254568b219553cc41a51
Petra Radocaj <radocajpetra@gmail.com>
2021-07-03 13:00:11 +0200

    Created admin login page

---
+++ public/admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="hr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+    <meta name="viewport" content="user-scalable = yes">
+    <link rel="stylesheet" href="./dist/style.css">
+
+    <title>MediLife</title>
+</head>
+
+<body class="admin-login-background">
+
+    <?php include "../src/components/admin-login.php"; ?>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" data-auto-a11y="true"></script>
+
+</body>
+
+</html>
