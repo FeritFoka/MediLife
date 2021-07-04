e94898c9bf10f83c9446574dff8d0b86ddabf9be
Stjepan Mrgnaić <stjepan.mrganic@gmail.com>
2021-07-04 20:17:15 +0200

    Fixed webpack not loading images and empty main.js

--- public/admin.php
+++ public/admin.php
@@ -15,8 +15,8 @@
 
     <?php include "../src/components/admin-login.php"; ?>
 
-	<script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" data-auto-a11y="true"></script>
-    <script src="../src/scripts/main.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" data-auto-a11y="true"></script>
+    <script src="./dist/bundle.js"></script>
 
 </body>
 
