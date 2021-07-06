5bf902ab2c8b971e12ffc7f76702acc302a0ba7e
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-06 23:13:07 +0200

    header & footer done

---
+++ src/components/dashboard/footer.php
@@ -0,0 +1,30 @@
+<?php
+
+    $appbar = new \Kendo\UI\AppBar('footer');
+    $appbar->position("bottom");
+    $appbar->positionMode('fixed');
+
+    $madeWithItem = new \Kendo\UI\AppBarItem();
+    $madeWithItem->type('contentItem');
+    $madeWithItem->template('
+    Web aplikacija je izrađena uz pomoć sljedećih tehnologija:
+    <ul>
+        <li>HTML5</li>
+        <li>PHP</li>
+        <li>Sass</li>
+        <li>Kendo UI for PHP</li>
+    </ul>
+    ');
+
+    $madeByItem = new \Kendo\UI\AppBarItem();
+    $madeByItem->type('contentItem');
+    $madeByItem->template('Web aplikaciju su napravili Stjepan Mrganić, Petra Radočaj i Dominik Ričko.');
+
+    $spacer = new \Kendo\UI\AppBarItem();
+    $spacer->type('spacer');
+
+    $appbar->addItem($madeWithItem);
+    $appbar->addItem($spacer);
+    $appbar->addItem($madeByItem);
+
+    echo $appbar->render();
---
+++ src/components/dashboard/header.php
@@ -0,0 +1,34 @@
+<?php
+
+    $appbar = new \Kendo\UI\AppBar('header');
+    $appbar->position("top");
+
+    $logoItem = new \Kendo\UI\AppBarItem();
+    $logoItem->type('contentItem');
+    $logoItem->template('<img src="./images/logo.png" width="150px", height="50px"');
+
+    $homeItem = new \Kendo\UI\AppBarItem();
+    $homeItem->type('contentItem');
+    $homeItem->template('<a href="./dashboard.php" class="navigation">Home</a>');
+
+    $findItem = new \Kendo\UI\AppBarItem();
+    $findItem->type('contentItem');
+    $findItem->template('<a href="./search.php" class="navigation">Find</a>');
+
+    $statsItem = new \Kendo\UI\AppBarItem();
+    $statsItem->type('contentItem');
+    $statsItem->template('<a href="./stats.php" class="navigation">Statistics</a>');
+
+    $spacer = new \Kendo\UI\AppBarItem();
+    $spacer->type('spacer');
+    $spacer->width(30.0);
+
+    $appbar->addItem($logoItem);
+    $appbar->addItem($spacer);
+    $appbar->addItem($homeItem);
+    $appbar->addItem($spacer);
+    $appbar->addItem($findItem);
+    $appbar->addItem($spacer);
+    $appbar->addItem($statsItem);
+
+    echo $appbar->render();
