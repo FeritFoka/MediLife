c792d8dc2638a045d47a078da588575c17cc9fdf
Petra Radocaj <radocajpetra@gmail.com>
2021-07-06 18:26:59 +0200

    Created index.php

---
+++ src/components/nav.php
@@ -0,0 +1,16 @@
+<section class="navbar navbar-default" role="navigation">
+    <div class="container">
+
+        <a role="link" href="#" class="navbar-brand">MediLife Health Center</a>
+
+        <ul class="nav navbar-nav navbar-right">
+            <li><a role="link" href="#">Home</a></li>
+            <li><a role="link" href="#medilife-team">Doctors</a></li>
+            <li><a role="link" href="#news">News</a></li>
+            <li><a role="link" href="#maps">Find us</a></li>
+            <li><a role="link" href="admin.php">Admin login</a></li>
+            <li class="appointment-link"><a role="link" href="send-email.php">Make an appointment</a></li>
+        </ul>
+
+    </div>
+</section>
