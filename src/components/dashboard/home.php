55ade8cd78d2e294255b0e668d2b26dd4e809e09
Dominik Ričko <dominik.ricko@gmail.com>
2021-07-07 20:10:06 +0200

    finished dashboard home

---
+++ src/components/dashboard/home.php
@@ -0,0 +1,8 @@
+<section class="section-dashboard-home">
+
+<h1 aria-readonly="true" class="row dashboard-title">Welcome to <span class="dashboard-home-title">Medilife</span> admin dashboard!</h1>
+
+<p aria-readonly="true" class="row dashboard-info">Your information</p>
+<p aria-readonly="true" class="row dashboard-info">Username: <span class="dashboard-username">Admin</span></p>
+
+</section>
